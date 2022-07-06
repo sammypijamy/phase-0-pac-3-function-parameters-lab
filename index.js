57cd54f2dde0befb90c59c6bf798a9521bbd9999
+ <root@SamWitch.localdomain>
2022-07-06 09:11:33 -0700

    commit tree a6755f463d6ac8f31e309b3a90763acac6e4a59f

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name)  {
+    return `Hi, my name is ${name}.`
+    name = `${Samip}, ${Aki}!`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+     name = ` ${Samip}, ${Aki}!`
+     language = `${Ember.js}!`
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    name = `${Samip}, ${Aki}!`
+    
+
+}
